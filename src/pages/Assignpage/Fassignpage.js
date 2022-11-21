92a872cc964c02f06380fc663c496754f71da1fe
Rashmith <rashmiths.191cs242@nitk.edu.in>
2022-11-21 21:07:42 +0530

    added error handling

--- src/pages/Assignpage/Fassignpage.js
+++ src/pages/Assignpage/Fassignpage.js
@@ -3,54 +3,63 @@ import React from "react";
 import { AssignUserToRole, GetUsersAndRoles, GetRolesForUnit } from "../../api/index";
 import Button from 'react-bootstrap/Button';
 import './assignpage.css';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 export default class Fassignpage extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            role: 'Role',
-            user: 'User',
-            userList: [],
-            roleList: [],
-            users:[],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: 'Role',
+      user: 'User',
+      userList: [],
+      roleList: [],
+      users: [],
     }
+  }
 
-    handleChange(e) {
-        this.setState({ [e.target.name] : e.target.value });
-     }
-  
-    handleSubmit = async () => {
-        let res = await AssignUserToRole(this.state.role, this.state.user, );
-        alert('Role ' + this.state.role + ' is assigned to ' + this.state.user);
-    }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    componentDidMount = () => {
-      GetUsersAndRoles().then((res) => {
-          if(res && res.data && res.data.users) {
-              let foundUsers = res.data.users;
-              let foundUserEmails = foundUsers.map(foundUser => foundUser.email);
-              this.setState({ userList: foundUserEmails});
-          }
-      });
-      GetRolesForUnit("ftpadmin").then((res) => {
-        if (res && res.data && res.data.users) {
-          let foundRoles = res.data.unitRoles;
-          let foundRoleslist = foundRoles.map(found => found.name);
-          this.setState({ roleList: foundRoleslist });
-          this.setState({ users: res.data.users });
-        }
-      });
-    }
-  
-    render() {
-      let {userList, users, roleList} = this.state;
-      console.log(userList);
-      console.log(roleList);
+  handleSubmit = async () => {
+    let res = await AssignUserToRole(this.state.role, this.state.user,);
+    console.log(res);
+    if (res.status == 200)
+      alert('Role ' + this.state.role + ' is assigned to ' + this.state.user);
+    else
+      alert('Failure: ' + res.data.message == null ? "Reason unknown" : res.data.message);
+  }
 
-      return (
-        <>
+  handleRemove = async () => {
+    let res = await AssignUserToRole(this.state.role, this.state.user,);
+    console.log(res);
+    alert('Role ' + this.state.role + ' is assigned to ' + this.state.user);
+  }
+  componentDidMount = () => {
+    GetUsersAndRoles().then((res) => {
+      if (res && res.data && res.data.users) {
+        let foundUsers = res.data.users;
+        let foundUserEmails = foundUsers.map(foundUser => foundUser.email);
+        this.setState({ userList: foundUserEmails });
+      }
+    });
+    GetRolesForUnit("ftpadmin").then((res) => {
+      if (res && res.data && res.data.users) {
+        let foundRoles = res.data.unitRoles;
+        let foundRoleslist = foundRoles.map(found => found.name);
+        this.setState({ roleList: foundRoleslist });
+        this.setState({ users: res.data.users });
+      }
+    });
+  }
+
+  render() {
+    let { userList, users, roleList } = this.state;
+    console.log(userList);
+    console.log(roleList);
+
+    return (
+      <>
         <div className="heading"> FTP Admin Page</div>
         <div className="master">
           <div className="user_role">
@@ -74,38 +83,41 @@ export default class Fassignpage extends React.Component {
           </div>
         </div>
 
-        <div className = "container">
+        <div className="container">
           Add Role to User?
           <form onSubmit={this.handleSubmit}>
-          <label className="labels">
-            <select name = "role" value={this.state.role}
-            onChange={this.handleChange.bind(this)}>
-              {
+            <label className="labels">
+              <select name="role" value={this.state.role}
+                onChange={this.handleChange.bind(this)}>
+                {
                   roleList.length && roleList.map(role => {
-                      return <option value={role}>{role}</option>
+                    return <option value={role}>{role}</option>
                   })
-              }
-            </select>
-          </label>
-          <label className="labels">
-            <select name = "user" value={this.state.user} 
-            onChange={this.handleChange.bind(this)}>
-              {
+                }
+              </select>
+            </label>
+            <label className="labels">
+              <select name="user" value={this.state.user}
+                onChange={this.handleChange.bind(this)}>
+                {
                   userList.length && userList.map(user => {
-                      return <option value={user}>{user}</option>
+                    return <option value={user}>{user}</option>
                   })
-              }
-            </select>
-          </label>
-          <div className="labels">
-            <Button onClick={(evt) => this.handleSubmit()} className="submitbutton">
-                  Submit
-            </Button>
-          </div>
+                }
+              </select>
+            </label>
+            <div className="labels">
+              <Button onClick={(evt) => this.handleSubmit()} className="submitbutton">
+                Add Role
+              </Button>
+              <Button onClick={(evt) => this.handleRemove()} className="submitbutton">
+                Remove Role
+              </Button>
+            </div>
           </form>
         </div>
-        </>
-        
-      );
-    }
+      </>
+
+    );
   }
+}
